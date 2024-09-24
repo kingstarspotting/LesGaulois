9ad55a1ca6b2b36e60bc979e657ecb6d0fe95aa4
kingstarspotting <nagr3@orange.fr>
2024-09-24 19:50:36 +0200

    correction 1

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -36,23 +36,31 @@ public class Romain {
 	}
 	
 	public void sEquiper(Equipement equipement) {
-		if (equipements[0] != null && equipements[1] != null) {
-			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
-		} else if (equipements[0] == equipement) {
-			System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
-		} else {
-			switch (equipement) {
-				case CASQUE: {
-					equipements[nbEquipement] = Equipement.CASQUE;
-					break;
+		switch (nbEquipement) {
+			case 2: {
+				System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+				break;
+			}
+			case 1: {
+				if (equipements[0] == equipement) {
+					System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
 				}
-				default:
-					equipements[nbEquipement] = Equipement.BOUCLIER;
-					break;
+				else {
+					equiper(equipement);
 				}
-			nbEquipement++;
-			System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+				break;
+			}
+			case 0:{
+				equiper(equipement);
+				break;
+			}
+			
 		}
+	}
+	private void equiper(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		nbEquipement++;
+		System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
 	}
 	
 	private boolean isForcePositive() {
